0c6588dd4f62863a4a4598acaec7dc66fd58ed89
Quaqva <sanek15056@gmail.com>
2024-02-18 20:35:09 +0400

    feat: close backend and db ports for production, forward API request from frontend to backend

--- frontend/src/app/interceptors/url.interceptor.ts
+++ frontend/src/app/interceptors/url.interceptor.ts
@@ -1,9 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
 
 const urlInterceptor: HttpInterceptorFn = (req, next) => {
-  const url = isDevMode() ? 'http://localhost:5134' : 'http://localhost:8080';
-  const newReq = req.clone({ url: `${url}/${req.url}` });
+  const url = isDevMode() ? 'http://localhost:5134' : '/api';
+  const newReq = req.clone({
+    url: `${url}/${req.url}`,
+    headers: new HttpHeaders({
+      'X-From-Frontend': 'true',
+    }),
+  });
   return next(newReq);
 };
 export default urlInterceptor;
